8e15c55225a2c92b2b9afa24d31a34441b44a6a6
Hannah0124 <hannahjoo24@gmail.com>
2019-05-23 18:50:05 -0700

    Interface and service

---
+++ tv-show-app/src/app/ishow-result.ts
@@ -0,0 +1,11 @@
+export interface IShowResult {
+  title: string
+  rating: number
+  genres: string
+  runtime: number
+  schedule: string
+  timezone: string
+  officialSite: string
+  summary: string
+  image: string
+}
--- tv-show-app/src/app/show-result/show-result.component.ts
+++ tv-show-app/src/app/show-result/show-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IShowResult } from '../ishow-result';
 
 @Component({
   selector: 'app-show-result',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-result.component.css']
 })
 export class ShowResultComponent implements OnInit {
+  tv: IShowResult
 
-  constructor() { }
+  constructor() {
+    // update dummy data based on http://api.tvmaze.com/search/shows?q=girls
+    this.tv = {
+      title: "Girls",
+      rating: 6.7,
+      genres: "Drama, Romance",
+      runtime: 30,
+      schedule: "22:00",
+      timezone: "America/New_York",
+      officialSite: "http://www.hbo.com/girls",
+      summary: "This Emmy winning series is a comic look at the assorted humiliations and rare triumphs of a group of girls in their 20s.",
+      image: "http://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg"
+    }
+  }
 
   ngOnInit() {
   }
